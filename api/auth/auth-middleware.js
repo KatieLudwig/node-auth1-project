77c3414d2a8832586d0e464ff2a42d48b5b34a72
Katie Ludwig <katierludwig@gmail.com>
2024-07-23 15:57:13 -0700

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -12,7 +12,7 @@ function restricted(req, res, next) {
   if(req.session && req.session.user) {
     next();
   } else {
-    res.status(401).json({ message: 'You shall not pass!' })''
+    res.status(401).json({ message: 'You shall not pass!' });
   }
 }
 
